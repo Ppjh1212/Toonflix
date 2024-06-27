807271b17ff1468cc4ee936ad4618a7b39594274
Ppjh1212 <145564710+Ppjh1212@users.noreply.github.com>
2024-06-27 23:45:26 +0900

    6

--- lib/test.dart
+++ lib/test.dart
@@ -1,45 +1,4 @@
-import 'package:flutter/material.dart';
-
+// ignore: avoid_print
 void main() {
-  runApp(App());
-}
-
-class App extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Color(0xFF181818),
-        body: Padding(
-          padding: EdgeInsets.all(10),
-          child: Column(
-          children: [
-            SizedBox(
-              height: 80,
-            ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.end,                
-                  children: [
-                    Text(
-                      'data 1',
-                      style: TextStyle(
-                        fontSize: 35,
-                      ),
-                    ), 
-                    Text('data 2'), 
-                    Text('data 3'), 
-                    Text('data 4'),
-                  ],
-                ),
-              ],
-            )
-          ],
-        ),
-        ),
-      ),
-    );
-  }
+  print('1234');
 }
