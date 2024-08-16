a2d933b95c1db9b59684172b6d0939b2f1cfc3e9
Ppjh1212 <145564710+Ppjh1212@users.noreply.github.com>
2024-08-16 23:18:18 +0900

    feat :: 24th

--- lib/webtoon_App/screens/detail_screen.dart
+++ lib/webtoon_App/screens/detail_screen.dart
@@ -1,10 +1,69 @@
 import 'package:flutter/material.dart';
 
 class DetailScreen extends StatelessWidget {
-  const DetailScreen({super.key});
+  final String title, thumb, id;
+
+  const DetailScreen({
+    super.key,
+    required this.title,
+    required this.thumb,
+    required this.id,
+  });
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        elevation: 2, //음영
+        backgroundColor: Colors.white,
+        foregroundColor: Colors.green,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              title,
+              style: const TextStyle(
+                fontSize: 24,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Column(
+        children: [
+          const SizedBox(
+            height: 50,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Hero(
+                tag: id,
+                child: Container(
+                  width: 230,
+                  clipBehavior: Clip.hardEdge,
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      boxShadow: [
+                        BoxShadow(
+                            blurRadius: 10, //그림자가 얼마나 멀리까지 드리울지
+                            offset: const Offset(7, 5), //그림자의 위치
+                            color: Colors.black.withOpacity(0.5) // 불투명도
+                            )
+                      ]),
+                  child: Image.network(
+                    thumb,
+                    headers: const {
+                      'Referer': 'https://comic.naver.com',
+                    },
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/webtoon_App/widgets/webtoon_widget.dart
+++ lib/webtoon_App/widgets/webtoon_widget.dart
@@ -1,3 +1,4 @@
+import 'package:Flutter_Study/webtoon_App/screens/detail_screen.dart';
 import 'package:flutter/material.dart';
 
 class Webtoon extends StatelessWidget {
@@ -15,27 +16,39 @@ class Webtoon extends StatelessWidget {
     return GestureDetector(
       // GestureDetector은 대부분의 동작을 감지함
       onTap: () {
-        //Navigator.push(context, route)
+        Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => DetailScreen(
+              title: title,
+              thumb: thumb,
+              id: id,
+            ),
+          ),
+        ); // Navigator로 route를 푸쉬, route는 DetailScreen을 렌더함
       },
       child: Column(
         children: [
-          Container(
-            width: 230,
-            clipBehavior: Clip.hardEdge,
-            decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(15),
-                boxShadow: [
-                  BoxShadow(
-                      blurRadius: 10, //그림자가 얼마나 멀리까지 드리울지
-                      offset: const Offset(7, 5), //그림자의 위치
-                      color: Colors.black.withOpacity(0.5) // 불투명도
-                      )
-                ]),
-            child: Image.network(
-              thumb,
-              headers: const {
-                'Referer': 'https://comic.naver.com',
-              },
+          Hero(
+            tag: id,
+            child: Container(
+              width: 230,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  boxShadow: [
+                    BoxShadow(
+                        blurRadius: 10, //그림자가 얼마나 멀리까지 드리울지
+                        offset: const Offset(7, 5), //그림자의 위치
+                        color: Colors.black.withOpacity(0.5) // 불투명도
+                        )
+                  ]),
+              child: Image.network(
+                thumb,
+                headers: const {
+                  'Referer': 'https://comic.naver.com',
+                },
+              ),
             ),
           ),
           const SizedBox(
